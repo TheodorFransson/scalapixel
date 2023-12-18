3f44940669c7b0a16989afedc13701099ab829bf
theodorfransson <theofransson0203@gmail.com>
2023-12-18 21:03:50 +0100

    Fixed JFXPanel to match the CanvasPanel in color and size.

--- src/main/scala/scalapaint/view/CanvasPanel.scala
+++ src/main/scala/scalapaint/view/CanvasPanel.scala
@@ -2,22 +2,24 @@ package scalapaint.view
 
 import javafx.application.Platform
 import javafx.embed.swing.{JFXPanel, SwingFXUtils}
-import javafx.scene.Scene
+import javafx.scene.{Scene, paint}
 import javafx.scene.canvas.Canvas
 import javafx.scene.image.WritableImage
 import scalapaint.*
 import scalapaint.image.{DrawnImage, EditorImage, RenderImage}
 
 import java.awt.image.BufferedImage
-import java.awt.{Color, Graphics2D, Image, Point}
+import java.awt.{BorderLayout, Color, FlowLayout, Graphics2D, Image, Point}
+import javax.swing.border.EmptyBorder
 import scala.swing.*
+import scala.swing.BorderPanel.Position.Center
 import scala.swing.Swing.*
 import scala.swing.event.*
 
 class CanvasPanel(initSize: Dimension, val padding: Int) extends Panel:
 	import CanvasPanel.Events.*
 
-	private val dim = new Dimension(initSize.width - (2 * padding), initSize.height - (2 * padding))
+	private val dim = new Dimension(initSize.width, initSize.height)
 	private val renderImage: RenderImage = new RenderImage(WritableImage(dim.width, dim.height))
 
 	val jfxPanel = new JFXPanel()
@@ -49,14 +51,26 @@ class CanvasPanel(initSize: Dimension, val padding: Int) extends Panel:
 		canvas.requestFocus()
 	})
 
+	val layout = new FlowLayout()
+	layout.setVgap(1)
+	peer.setLayout(layout)
+
 	updateSize(initSize, padding)
 	background = Colors.backgroundColorDP(0)
 	focusable = true
 
 	def updateSize(size: Dimension, padding: Int): Unit =
 		preferredSize = new Dimension(size.width, size.height)
+		jfxPanel.setPreferredSize(new Dimension(size.width, size.height))
+
+		Platform.runLater(() => {
+			canvas.setWidth(jfxPanel.getWidth)
+			canvas.setHeight(jfxPanel.getHeight)
+		})
+
 		revalidate()
 		EditorWindow.pack()
+		drawImageOnCanvas()
 		repaint()
 
 	override def paintComponent(g: Graphics2D): Unit =
@@ -73,6 +87,10 @@ class CanvasPanel(initSize: Dimension, val padding: Int) extends Panel:
 	def drawImageOnCanvas(): Unit =
 		Platform.runLater(() => {
 			val graphicsContext = canvas.getGraphicsContext2D
+
+			graphicsContext.setFill(new javafx.scene.paint.Color(background.getRed().toDouble / 255.0, background.getBlue().toDouble / 255.0, background.getGreen().toDouble / 255.0, 1.0)) // Set your desired background color here
+			graphicsContext.fillRect(0, 0, canvas.getWidth, canvas.getHeight)
+
 			renderImage.render(graphicsContext)
 		})
 
@@ -83,6 +101,13 @@ class CanvasPanel(initSize: Dimension, val padding: Int) extends Panel:
 	def pan(dx: Int, dy: Int): Unit =
 		renderImage.pan(dx, dy)
 		drawImageOnCanvas()
+
+	listenTo(this)
+	reactions += {
+		case UIElementResized(_) =>
+			val newSize = peer.getSize()
+			updateSize(new Dimension(newSize.width, newSize.height), padding)
+	}
 
 object CanvasPanel:
 	object Events:
