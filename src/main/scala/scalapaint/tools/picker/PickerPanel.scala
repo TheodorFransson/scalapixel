075bae054c67d246de15f747c653a4e052a6d6d4
theodorfransson <theofransson0203@gmail.com>
2024-02-17 15:10:07 +0100

    Added a placeholder description to PickerPanel

--- src/main/scala/scalapaint/tools/picker/PickerPanel.scala
+++ src/main/scala/scalapaint/tools/picker/PickerPanel.scala
@@ -2,6 +2,18 @@ package scalapaint.tools.picker
 
 import scalapaint.tools.ToolPanel
 
-import scala.swing.Panel
+import javax.swing.border.EmptyBorder
+import scala.swing.{Alignment, Panel, TextArea, TextField}
 
-class PickerPanel extends ToolPanel("Color picker")
+class PickerPanel extends ToolPanel("Color picker"):
+	private val description = new TextArea():
+		text =
+			"""Use the left mouse button to set
+				|the primary color, use the right
+				|mouse button to set the
+				|secondary color.""".stripMargin
+		editable = false
+		columns = 10
+		rows = 10
+
+	contents += description
